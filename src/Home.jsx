8173c339139828ce44b4c5d1d546f9d607dcb5af
iamabdur <abdurrahmank2002@gmail.com>
2023-09-04 15:02:54 +0530

    css file updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,9 +15,9 @@ function Home() {
           <h1 className='todo' style={{ color: 'white' }}>To-Do List</h1>
           <p style={{ color: 'white' }}>Stay Organized, Achieve More.</p>
           <br />
-          <button className='button'><Link to="/dashboard" style={{ color: 'white' }} >Start Sticky</Link></button><br></br>
+          <button className='button'><Link to="/dashboard" style={{ color: 'white' }} >Start Sticky Notes</Link></button><br></br>
           
-          <button  className='button b1' ><Link to="/todo" style={{ color: 'white' }} >Start ToDo</Link></button>
+          <button  className='button b1' ><Link to="/todo" style={{ color: 'white' }} >Start ToDo List</Link></button>
         </div>
       </div>
       
