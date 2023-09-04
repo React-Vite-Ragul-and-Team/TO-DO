e3d57f98945096dc17554abc2c98070684dc739b
iamabdur <abdurrahmank2002@gmail.com>
2023-09-04 12:03:05 +0530

    hello  this is my first commit

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,21 +1,75 @@
-import React,{Component} from 'react';
+import React, { useState } from 'react';
 
-class Dashboard extends Component{
+function Dashboard() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [editingIndex, setEditingIndex] = useState(-1);
 
-  constructor(props){
-    super(props)
-    this.list = ["one","two","three","four"];
-  }
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      if (editingIndex === -1) {
+        setTodos([...todos, { text: input, completed: false }]);
+      } else {
+        const updatedTodos = [...todos];
+        updatedTodos[editingIndex] = { text: input, completed: false };
+        setTodos(updatedTodos);
+        setEditingIndex(-1);
+      }
+      setInput('');
+    }
+  };
 
-  render(){
-    return(
-      <>
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  const editTodo = (index) => {
+    const todoToEdit = todos[index];
+    setInput(todoToEdit.text);
+    setEditingIndex(index);
+  };
+
+  const toggleCompletion = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>
+        {editingIndex === -1 ? 'Add' : 'Update'}
+      </button>
+      <div className="navbar">
         <ul>
-          {this.list.map((l,i) => <li key={i} >{l}</li> )}
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <span
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none',
+                }}
+              >
+                {todo.text}
+              </span>
+              <button onClick={() => toggleCompletion(index)}>
+                {todo.completed ? 'Undo' : 'Complete'}
+              </button>
+              <button onClick={() => editTodo(index)}>Edit</button>
+              <button onClick={() => removeTodo(index)}>Delete</button>
+            </li>
+          ))}
         </ul>
-      </>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/Router.jsx
+++ src/Router.jsx
@@ -10,6 +10,7 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="dashboard" element={<Dashboard />} />
+
                 <Route path="*" element={<ErrorPage />} />
             </Routes>
         </BrowserRouter>
